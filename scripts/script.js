050a014d9d015e11ff9c6ab530489d9c671e4a03
Vladimir Metelev <metelev_vladimir@mail.ru>
2023-02-07 21:06:11 +0300

    внесены правки

--- scripts/script.js
+++ scripts/script.js
@@ -4,26 +4,28 @@ let name = document.querySelector('.profile__name');
 let description = document.querySelector('.profile__description');
 let edit_button = document.querySelector('.profile__edit-button');
 let exit_button = popup.querySelector('.popup__exit-button');
-let popup_name = popup.querySelector('.popup__name')
-let popup_desc = popup.querySelector('.popup__description')
+let popup_name = popup.querySelector('#userName');
+let popup_desc = popup.querySelector('#userDescription');
 let save_button = popup.querySelector('.popup__save-button');
-let like_button = document.querySelectorAll('.elements__like-button');
+
 popup_name.value = name.textContent;
 popup_desc.value = description.textContent;
 
 togglePopup();
 
+function stopDefAction(evt) {
+    evt.preventDefault();
+}
 function togglePopup() {
     popup.classList.toggle('popup_opened');
 }
-function saveInfo() {
+function saveInfo(evt) {
+    evt.preventDefault()
     name.textContent = popup_name.value;
     description.textContent = popup_desc.value;
     togglePopup();
 }
+
 edit_button.addEventListener('click', togglePopup);
 exit_button.addEventListener('click', togglePopup);
 save_button.addEventListener('click', saveInfo);
-
-
-
